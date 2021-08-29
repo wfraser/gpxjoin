8bb3407cdc2dcf77cdefc1f1117c4a1c42da1742
Bill Fraser <wfraser@codewise.org>
2021-08-29 13:25:11 -0700

    more proper argument parsing, with usage text, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::{bail, Context as _};
 use quick_xml::{Reader, Writer};
 use quick_xml::events::Event;
-use std::path::Path;
+use std::path::PathBuf;
 
 trait StartsWithExt<U> {
     fn starts_with(&self, other: U) -> bool;
 }
 
+// Because the stdlib's slice::starts_with() doesn't work when the inner type is Vec<u8> and we're
+// comparing against &[u8].
 impl<S: AsRef<[u8]>> StartsWithExt<&[S]> for Vec<Vec<u8>> {
     fn starts_with(&self, other: &[S]) -> bool {
         if self.len() < other.len() {
@@ -21,12 +23,41 @@ impl<S: AsRef<[u8]>> StartsWithExt<&[S]> for Vec<Vec<u8>> {
     }
 }
 
+fn parse_args() -> Vec<PathBuf> {
+    let mut paths = vec![];
+    let mut ignore_flags = false;
+    for arg in std::env::args_os().skip(1) {
+        if !ignore_flags {
+            match arg.to_str() {
+                Some("-h") | Some("--help") | Some("-V") | Some("--version") => {
+                    eprintln!("gpxjoin v{} (c) 2021 {}",
+                        env!("CARGO_PKG_VERSION"),
+                        env!("CARGO_PKG_AUTHORS"));
+                    eprintln!("usage: {} <file1.gpx> [<file2.gpx>, ...] > out.gpx",
+                        std::env::args().next().unwrap());
+                    eprintln!("Concatenates GPX files by appending tracks from subsequent GPX \
+                        files after tracks from the first.\n\
+                        Writes result to standard output.");
+                    std::process::exit(1);
+                }
+                Some("--") => {
+                    ignore_flags = true;
+                    continue;
+                }
+                _ => ()
+            }
+        }
+        paths.push(PathBuf::from(arg));
+    }
+    paths
+}
+
 fn main() -> anyhow::Result<()> {
     let mut writer = Writer::new(std::io::stdout());
     let mut first = None;
     let mut buf = vec![];
-    for path in std::env::args_os().skip(1) {
-        let mut r = Reader::from_file(&Path::new(&path))
+    for path in parse_args() {
+        let mut r = Reader::from_file(&path)
             .with_context(|| format!("failed to open {:?}", path))?;
 
         let mut path = vec![];
@@ -40,14 +71,17 @@ fn main() -> anyhow::Result<()> {
                     path.push(start.name().to_owned());
                 }
                 Event::End(_) => {
-                    if path == &[b"gpx"] {
-                        // Done with tracks, save the first file reader at this point.
+                    if path == [b"gpx"] {
+                        // Done with tracks, save the first file reader at this point and move on
+                        // to the next file.
                         first = Some((r, evt.into_owned()));
                         break;
                     }
                 }
                 _ => (),
             }
+            // If this is the first file, write everything, otherwise only write events if our path
+            // begins with a track element.
             if first.is_none() || path.starts_with(&[b"gpx", b"trk"]) {
                 writer.write_event(&evt)?;
             }
@@ -64,6 +98,7 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
+    // Finish writing out the first file.
     let (mut first, stashed_evt) = first.unwrap();
     writer.write_event(stashed_evt)?;
     loop {
